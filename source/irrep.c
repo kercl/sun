7f9593cc4d1b52004eb908429a3fd253f032c1a0
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2019-06-01 19:52:53 +0200

    started work on lowering operators; removed add function

--- source/irrep.c
+++ source/irrep.c
@@ -11,19 +11,13 @@ void _sigma(int *result, int *patterns, size_t n_patterns, size_t length, int l)
     }
 }
 
-void _add(int *result, int *a, int *b, size_t length) {
-    for(size_t i = 0; i < length; i++) {
-        result[i] = a[i] + b[i];
-    }
-}
-
 void gt_center_generator(struct gt_tree *patterns, int l, int *numerator, int *denominator) {
     size_t length = patterns->length,
            num_patterns = patterns->num_patterns;
     int *pattern_array = patterns->array_representation;
 
-    size_t n_entries = (length * (length + 1)) / 2;
-    size_t row_start = n_entries - ((length - l) * (length - l + 1)) / 2;
+    size_t n_entries = (length * (length + 1)) / 2,
+           row_start = n_entries - ((length - l) * (length - l + 1)) / 2;
 
     for(size_t M = 0, Mi; M < num_patterns * n_entries; M+=n_entries, Mi++) {
         numerator[Mi] = 0;
@@ -37,5 +31,20 @@ void gt_center_generator(struct gt_tree *patterns, int l, int *numerator, int *d
             numerator[Mi] -= pattern_array[M + row_start + j - (length - l) - 1];
         }
         denominator[Mi] = 2;
+    }
+}
+
+void gt_lowering_operator(struct gt_tree *patterns, int l, int *numerator, int *denominator) {
+    size_t length = patterns->length,
+           num_patterns = patterns->num_patterns;
+    int *pattern_array = patterns->array_representation;
+
+    size_t n_entries = (length * (length + 1)) / 2,
+           Mj;
+
+    for(size_t M = 0, Mi; M < num_patterns * n_entries; M+=n_entries, Mi++) {
+        for(size_t k = length; k < n_entries; k++) {
+            
+        }
     }
 }
